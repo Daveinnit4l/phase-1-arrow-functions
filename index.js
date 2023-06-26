bca2141056efc3e969ebf6fbe5c321a5b24462aa
kyle <davek2647@gmail.com>
2023-06-26 10:07:25 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide(){
+    let num1=2000
+    let num2=100
+    return (num1/num2);
+
+}
+const square=(parameter=32) => parameter*parameter;
+
+const add =(parameter1, parameter2) => parameter1+parameter2;
+add(4,12);
